65dc2bc6e9917eac444d73decaa68908e8905e9c
embunazzahra <dhauembunazzahra@gmail.com>
2021-12-04 15:49:07 +0700

    adding tablayout

--- app/src/main/java/com/DhauEmbunAzzahraJmartPK/MainActivity.java
+++ app/src/main/java/com/DhauEmbunAzzahraJmartPK/MainActivity.java
@@ -1,24 +1,58 @@
 package com.DhauEmbunAzzahraJmartPK;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.DhauEmbunAzzahraJmartPK.model.Account;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 pager2;
+    FragmentAdapter adapter;
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView hello_user = findViewById(R.id.textView);
+        //Account account = LoginActivity.getLoggedAccount();
 
-        Account account = LoginActivity.getLoggedAccount();
+        tabLayout = findViewById(R.id.tab_layout);
+        pager2 = findViewById(R.id.view_pager2);
 
-        hello_user.setText("Hello, "+account.name);
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm,getLifecycle());
+        pager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Products"));
+        tabLayout.addTab(tabLayout.newTab().setText("Filter"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
