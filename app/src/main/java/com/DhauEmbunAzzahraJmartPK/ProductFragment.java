1c2f71283218b9716db6ac284d06d94d6178896d
embunazzahra <dhauembunazzahra@gmail.com>
2021-12-13 00:55:06 +0700

    finish the product fragment

--- app/src/main/java/com/DhauEmbunAzzahraJmartPK/ProductFragment.java
+++ app/src/main/java/com/DhauEmbunAzzahraJmartPK/ProductFragment.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
@@ -46,10 +47,14 @@ import java.util.List;
 
 
 public class ProductFragment extends Fragment {
+    final int pageSize = 10;
+    int page = 0;
 
     private static final Gson gson = new Gson();
     private static ArrayList<Product> productsList = new ArrayList<>();
     ListView listProd;
+    EditText etPage;
+    Button prevBtn, nextBtn, goBtn;
     public static Product selectedProduct = null;
     public static Product getProductDetail(){
         return selectedProduct;
@@ -92,7 +97,12 @@ public class ProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View productView = inflater.inflate(R.layout.fragment_product, container, false);
+        etPage = (EditText) productView.findViewById(R.id.etPage);
+        prevBtn = (Button) productView.findViewById(R.id.button4);
+        nextBtn = (Button) productView.findViewById(R.id.button5);
+        goBtn = (Button) productView.findViewById(R.id.button6);
         listProd = (ListView) productView.findViewById(R.id.lvProduct);
+
         listProd.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
@@ -103,10 +113,6 @@ public class ProductFragment extends Fragment {
             }
         });
 
-
-
-        final int pageSize = 20;
-        int page = 0;
 
         Response.Listener<String> listener = new Response.Listener<String>() {
             @Override
@@ -121,16 +127,72 @@ public class ProductFragment extends Fragment {
                                 android.R.layout.simple_list_item_1,
                                 productsList
                         );
-
-
-                        listProd.setAdapter(listViewAdapter);
+                        if(!productsList.isEmpty())
+                            listProd.setAdapter(listViewAdapter);
+                        else
+                            if(page>0)
+                                page--;
+                        Toast.makeText(getActivity(), String.valueOf(page),Toast.LENGTH_SHORT).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
             }
         };
+
+        Response.Listener<String> secondListener = response -> {
+            try {
+                JSONArray object = new JSONArray(response);
+                if (object != null) {
+                    productsList = gson.fromJson(object.toString(), new TypeToken<ArrayList<Product>>() {
+                    }.getType());
+                    ArrayAdapter<Product> listViewAdapter = new ArrayAdapter<Product>(
+                            getActivity(),
+                            android.R.layout.simple_list_item_1,
+                            productsList
+                    );
+                    listProd.setAdapter(listViewAdapter);
+                    Toast.makeText(getActivity(), String.valueOf(page),Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        };
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                page++;
+                RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                requestQueue.add(RequestFactory.getPage("product", page, pageSize, listener, null));
+
+            }
+        });
+
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(page>0){
+                    page--;
+                    RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                    requestQueue.add(RequestFactory.getPage("product", page, pageSize, listener, null));
+                }
+            }
+        });
+
+        goBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    page = Integer.parseInt(etPage.getText().toString());
+                    RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                    requestQueue.add(RequestFactory.getPage("product", page, pageSize, secondListener, null));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getActivity(), "failed to parse.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         requestQueue.add(RequestFactory.getPage("product", page, pageSize, listener, null));
         return productView;
